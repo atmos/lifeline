65ea16f67f3d8b4f3b6a47ee99fade178170149f
Corey Donohoe <atmos@atmos.org>
2009-04-09 05:25:00 -0600

    remove the ruby time ago in words in favor of the js version, shift js out to its own template file of sorts we don't run into annoying regex escape issues again

--- lib/lifeline/sinatra/app.rb
+++ lib/lifeline/sinatra/app.rb
@@ -12,24 +12,6 @@ module Lifeline
       def current_user
         session[:user_id].nil? ? nil : ::Lifeline::User.get(session[:user_id])
       end
-
-      def distance_of_time_in_words(from_time, to_time)
-        distance_in_minutes = ((to_time - from_time) / 60).round
-
-        case distance_in_minutes
-          when 0          then "less than a minute"
-          when 1          then "1 minute"
-          when 2..45      then "#{distance_in_minutes} minutes"
-          when 46..90     then "about 1 hour"
-          when 90..1440   then "about #{(distance_in_minutes.to_f / 60.0).round} hours"
-          when 1441..2880 then "1 day"
-          else                 "#{(distance_in_minutes / 1440).round} days"
-        end
-      end
-
-      def distance_of_time_in_words_to_now(from_time)
-        distance_of_time_in_words(Time.parse(from_time), Time.now)
-      end
     end
 
     error do
@@ -63,46 +45,7 @@ module Lifeline
     end
 
     get '/application.js' do
-      <<-EOF
-$(function() {
-  function time_ago_in_words(timestamp_id) {
-    var now = (new Date).getTime();
-    var timestamp = timestamp_id.replace(/created_at_\\d+_/, '');
-    var result = null;
-    if(timestamp.match(/\\d+/)) {
-      var dt = Math.round(parseInt((now / 1000) - timestamp)/60);
-      if(dt == 0) { result = "less than a minute"; }
-      else if(dt == 1) { result = "1 minute"; }
-      else if(dt >= 2 && dt <= 45) { result = dt + ' minutes'; }
-      else if(dt >= 46 && dt <= 90) { result = 'about 1 hour'; }
-      else if(dt >= 90 && dt <= 1440) { result = 'about ' + Math.round(parseFloat(dt)/60.0) + ' hours'; }
-      else if(dt >= 1440 && dt <= 2880) { result = '1 day'; }
-      else { result = Math.round(parseInt(dt)/1440) + ' days'; }
-    }
-    return(result);
-  }
-  function refresh_page(since_id) {
-    $.get("/refresh/"+since_id, function(html) {
-      $('ol.statuses').prepend(html)
-      $(html).show('Explode',{},500);
-    });
-    var i = 0;
-    $('ol li span.entry-meta').each(function() {
-      if(i += 1 > 25) {
-        $(this).remove;
-      } else {
-        var result = time_ago_in_words(this.id);
-        var content = $(this).html().replace(/.*? ago/, result+ ' ago');
-        $(this).html(content);
-      }
-    });
-  };
-
-  $('ol.statuses').everyTime(50000, function(i) {
-    refresh_page($('ol.statuses li')[0]['id'].replace('status_', ''));
-  })
-});
-      EOF
+      @application_js ||= File.read(File.dirname(__FILE__)+'/views/application.js')
     end
 
     get '/signup' do
